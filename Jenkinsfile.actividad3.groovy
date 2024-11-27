445c913857168570b329767777d891c94646433e
ivanscubillosb <149205354+ivanscubillosb@users.noreply.github.com>
2024-11-26 22:31:46 -0500

    Create Jenkinsfile.actividad3.groovy

---
+++ Jenkinsfile.actividad3.groovy
@@ -0,0 +1,30 @@
+pipeline {
+    agent {
+        label 'docker'
+    }
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building stage!'
+                sh 'make build'
+            }
+        }
+        stage('API tests') {
+            steps {
+                sh 'make test-api'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
+        stage('E2E tests') {
+            steps {
+                sh 'make test-e2e'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
+    }
+    post {
+        always {
+            junit 'results/*_result.xml'
+        }
+    }
+}
